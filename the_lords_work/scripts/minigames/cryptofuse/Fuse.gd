387662a38a480d20d4b674611bee747aebaddda0
Jaskin <jaskinkabir@gmail.com>
2025-02-24 17:28:15 -0500

    updated fuse script

--- the_lords_work/scripts/minigames/cryptofuse/Fuse.gd
+++ the_lords_work/scripts/minigames/cryptofuse/Fuse.gd
@@ -3,11 +3,11 @@ extends Node2D
 @export var cover_color: Color
 @export var cover_opacity: float
 @export var type: int
+@export var state: bool
 
 func set_type(t: int):
 	type = t
 	$Type_text.text = "C%d" % t
-
 
 func set_color(c: Color):
 	cover_color = c
@@ -16,10 +16,17 @@ func set_color(c: Color):
 func set_opacity(a: float):
 	cover_color.a = a
 	$Cover.color = cover_color
+
+func set_state(s: bool):
+	state = s
+	$Wire.frame
+
 func randomize():
 	var colors = [Color.BLUE, Color.GREEN, Color.RED, Color.PURPLE, Color.YELLOW]
 	var color_choice = colors[randi() % colors.size()]
 	set_color(color_choice)
+	var t = ((randi() % 9) + 1) * 10
+	set_type(t)
 	
 	
 
